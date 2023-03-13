22f6c7ee9dce9c2d7fd549c05e60d98ad67e65c1
Antoine Fremon <antoine.fremon@flutilliant.com>
2023-03-13 16:50:08 +0100

    BE - Leisure bases - find coordinates if not given

--- backend/src/controllers/leisures.js
+++ backend/src/controllers/leisures.js
@@ -1,3 +1,5 @@
+const axios = require('axios');
+
 const Leisure = require('../models/Leisure');
 const Activity = require('../models/Activity');
 
@@ -7,6 +9,13 @@ module.exports = {
 };
 
 function createLeisure(activitiesId, name, description, address, coordinates, webLink) {
+    if (!coordinates) {
+        return getCoordinates(address)
+            .then((foundCoordinates) => {
+                return createLeisure(activitiesId, name, description, address, foundCoordinates, webLink);
+            });
+    }
+
     return Leisure.create({
         name,
         description,
@@ -24,4 +33,11 @@ function createLeisure(activitiesId, name, description, address, coordinates, we
 
 function getLeisures(pageSize = 20, offset = 0) {
     return Leisure.findAll({ limit: pageSize, offset, include: 'Activities' });
+}
+
+function getCoordinates(address) {
+    return axios.get(`https://api.mapbox.com/geocoding/v5/mapbox.places/${address}.json?access_token=${process.env.MAPBOX_TOKEN}`)
+        .then((response) => {
+            return Promise.resolve(response.data.features[0].geometry.coordinates[0] + ',' + response.data.features[0].geometry.coordinates[1]);
+        });
 }
