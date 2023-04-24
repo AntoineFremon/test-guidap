cf40d12fbfa6216775dc41b1fd9f5b4c9bcbef0d
Antoine Fremon <antoine.fremon@flutilliant.com>
2023-04-24 22:04:54 +0200

    FE - weather icon & popover

--- frontend/src/components/LeisureCard.vue
+++ frontend/src/components/LeisureCard.vue
@@ -1,6 +1,16 @@
 <template>
   <b-card v-on:click="select" :border-variant="border">
-    <b-card-title>{{leisure.name}}</b-card-title>
+    <b-card-title>
+      <b-row>
+        <b-col cols="10">{{leisure.name}}</b-col>
+        <b-col cols="2">
+          <img :src="`http://openweathermap.org/img/wn/${leisure.weatherIcon}@2x.png`" :style="{ 'height': '40px', 'width': '40px' }" :id="`popover-target-${leisure.id}`">
+          <b-popover :target="`popover-target-${leisure.id}`" triggers="hover" placement="top">
+            {{leisure.weatherDescription}}
+          </b-popover>
+        </b-col>
+      </b-row>
+    </b-card-title>
     <b-card-text>{{leisure.description}}</b-card-text>
     <b-button v-if="leisure.webLink" :href="leisure.webLink" variant="link" target="_blank">Accédez au site web</b-button>
     <b-badge v-for="a in leisure.Activities">{{a.name}}</b-badge>
